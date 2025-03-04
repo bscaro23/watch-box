870060e0e4351ce916cc754ee1552facf4be270c
Bscar <oscarosaurus@gmail.com>
2025-03-04 16:50:57 +0000

    completed login and signup functionality

--- watchbox/settings.py
+++ watchbox/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGOUT_REDIRECT_URL = 'home'
+
+# todo LOGIN_REDIRECT_URL = 'cat-index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
